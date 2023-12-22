1c5ce9cd7d5c3340233936886ea4caa17b4c3638
Alfred Otchere <98951880+alfrodark@users.noreply.github.com>
2023-12-22 12:14:36 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig:{
-    apiKey: "AIzaSyChWaAGWLtItnI7RsomJEv6mXd_0yd0yxI",
-    authDomain: "financeappproject.firebaseapp.com",
-    databaseURL: "https://financeappproject-default-rtdb.firebaseio.com",
-    projectId: "financeappproject",
-    storageBucket: "financeappproject.appspot.com",
-    messagingSenderId: "116963876026",
-    appId: "1:116963876026:web:e0cce4113f46fab1a8b683",
-    measurementId: "G-DKF1GH8B97",
+   
   }
 };
 
